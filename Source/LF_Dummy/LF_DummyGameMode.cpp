80a9a7b119fb4c66fd17925a647ca1954b36002f
Dae work PC <dae.lee@interaptix.com>
2017-05-29 14:08:29 -0400

    initial commit

---
+++ Source/LF_Dummy/LF_DummyGameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "LF_Dummy.h"
+#include "LF_DummyGameMode.h"
+
+
+
+
---
+++ Source/LF_Dummy/LF_DummyGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "LF_DummyGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LF_DUMMY_API ALF_DummyGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
